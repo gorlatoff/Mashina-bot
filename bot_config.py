b437c104160acfedf36bb70f74accd5decf01fb9
gorlatoff <48055313+gorlatoff@users.noreply.github.com>
2021-09-27 18:20:20 +0800

    make it better

--- bot_config.py
+++ bot_config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODI2NDQyNzI2NTU5MzE4MDI2.YGMixA.y0OO-oxyo2CCHssX9ZS_g3QMJC8',
+    'token': 'ODI2NDQyNzI2NTU5MzE4MDI2.YGMixA.nDLXJ1KgyYcSF8o44G98S-YDTh4',
     'bot': 'Mašina',
     'id': 826442726559318026,
     'prefix': '.'
